0920f1e3277d9ba91b3ed0850666dc7204198368
werner <werner@frontwise.com>
2017-11-23 12:19:36 +0100

    sorted components

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,29 @@
 import App from './App/App';
+import About from '../About/About';
 import BreadCrumbs from './BreadCrumbs/BreadCrumbs';
+import Contact from '../Contact/Contact';
+import Data from '../Data/Data';
+import Docs from '../Docs/Docs';
 import Elements from './Elements/Elements';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Help from './Help/Help';
 import Homepage from './Homepage/Homepage';
 import Page from './Page/Page';
+import Tools from '../Tools/Tools';
 
 export{
+  About,
   App,
   BreadCrumbs,
+  Contact,
+  Data,
+  Docs,
   Elements,  
   Footer,
   Header,
-  Help,
+  Help,  
   Homepage,
   Page,
+  Tools
 }
