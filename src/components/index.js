2f0e35dd8812c8bc07af84ad5bd213d89c6afc2b
werner <werner@frontwise.com>
2017-11-23 12:22:05 +0100

    fix paths

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,16 @@
 import App from './App/App';
-import About from '../About/About';
+import About from './About/About';
 import BreadCrumbs from './BreadCrumbs/BreadCrumbs';
-import Contact from '../Contact/Contact';
-import Data from '../Data/Data';
-import Docs from '../Docs/Docs';
+import Contact from './Contact/Contact';
+import Data from './Data/Data';
+import Docs from './Docs/Docs';
 import Elements from './Elements/Elements';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Help from './Help/Help';
 import Homepage from './Homepage/Homepage';
 import Page from './Page/Page';
-import Tools from '../Tools/Tools';
+import Tools from './Tools/Tools';
 
 export{
   About,
